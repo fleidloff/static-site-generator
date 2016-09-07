5bf6b7c86fcc5b029b53e308e3187edc7fca2840
Frederik Leidloff <fleidloff@team.mobile.de>
2016-09-07 11:07:19 +0200

    compile less files only if destination file does not exist, yet

--- generator/delegator/less.js
+++ generator/delegator/less.js
@@ -1,5 +1,6 @@
 const Delegator = require("./delegator");
 const path = require("path");
+const fs = require("fs");
 
 const spawn = require('child_process').spawn;
 
@@ -9,9 +10,9 @@ class Css extends Delegator {
         if (name === "less" && attrs && typeof attrs.src === "string") {
             const { source, destination } = this.config;
             const file = attrs.src.replace(".less", ".css");
-
-            const less = spawn("./node_modules/.bin/lessc", [source.path + attrs.src, destination.path + file]);
-
+            if (!fs.existsSync(destination.path + file)) {
+                const less = spawn("./node_modules/.bin/lessc", [source.path + attrs.src, destination.path + file]);
+            }
             return `<link rel="stylesheet" href="${file}" />\n`;
         } else {
             throw new Error("<css> tag must have a src attribute.");
