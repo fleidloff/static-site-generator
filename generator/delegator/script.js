47b73d35f963b5cc7a497e51730cf3e22a4924ec
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-09-18 11:17:19 +0200

    .

--- generator/delegator/script.js
+++ generator/delegator/script.js
@@ -6,7 +6,7 @@ const Html = require("./html");
 
 class Script extends Html {
 	onopentag(name, attrs) {
-        if (name === "script" && attrs && typeof attrs.src === "string") {
+        if (name === "script" && attrs && typeof attrs.src === "string" && attrs.src.indexOf("http") !== 0) {
         	const { source, destination } = this.config;
             copyFiles({ files: attrs.src, source, destination });
             const fileNames = addHash({ files: attrs.src, source, destination });
